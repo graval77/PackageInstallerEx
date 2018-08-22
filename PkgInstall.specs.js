228c405e86369678f84410f7736e2a0b73c251a0
Gopesh Raval <graval77@gmail.com>
2018-08-22 13:31:02 -0400

    Checked few more cases for validation of the input

--- PkgInstall.specs.js
+++ PkgInstall.specs.js
@@ -21,4 +21,41 @@ describe('Package Installer', function() {
       expect(function() { new packageInstallerEx([{a:'b'},{c:'d'}]); }).toThrow();
     });
   });
+
+  describe('Success', function() {
+    it('when the input contains an array of single string', function() {
+      var input = ['a:'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('a');
+    });
+  
+   it('when the input contains many individual packages', function() {
+      var input = ['a:','b:','c:','d:','e:'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('a, b, c, d, e');
+    });
+
+   it('when the input is not in order', function() {
+      var input = ['b:a','c:b','a:'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('a, b, c');
+    });
+
+   it('when a package is a dependency of multiple packages', function() {
+      var input = ['a:','b:a','c:a','d:c','e:c'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('a, b, c, d, e');
+    });
+  
+   it('when a package name contains special characters', function() {
+      var input = ['{###}:'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('{###}');
+    });
+    
+    it('when the input contains duplication of package dependencies', function() {
+      var input = ['a:','b:', 'b:a'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('a, b');
+    });
+
+    it('when a package name contains two words separated by space', function() {
+      var input = ['Kitten Service:Camel Caser', 'Camel Caser:'];
+      expect(new packageInstallerEx(input).dependencyCheck()).toEqual('Camel Caser, Kitten Service');
+    });  
+  });
 });
