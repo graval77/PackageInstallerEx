841f2ab8e3dbb87184f950447d85e54f41c12ad0
Gopesh Raval <graval77@gmail.com>
2018-08-22 13:34:23 -0400

    Added the function which validates input packages and also the function which sorts dependencies

--- PkgInstall.js
+++ PkgInstall.js
@@ -10,4 +10,56 @@ var packageInstallerEx = function (input) {
     } 
   }
 
+/*Validation of Individual Packages*/
+  var individualPackages = function() {
+    var output = {};
+      for(var i=0;i<input.length;i++){
+        var values=input[i].split(':');
+        var package = values[0].trim();
+        var dependency = values[1].trim();
+        if (package.length === 0) {
+          throw 'Invalid Input: Please enter a Package Name';
+        }
+        if (!output[package]){
+          output[package] = [];
+        } 
+        if (!output[dependency] && dependency.length > 0){
+          output[dependency] = [];
+        } 
+        if (dependency.length > 0){
+          output[package].push(dependency);
+        }
+      }
+    return output;
+  }
+
+ /*Ordering according to dependencies*/
+  var orderOfInstall = function(indvlPkgs) {
+    var output = []; 
+    var ordered = {};
+    for(var i=0;i<Object.keys(indvlPkgs).length;i++){
+      sort(Object.keys(indvlPkgs)[i], []);
+    }
+    function sort(package, mainPkg) {
+      if (ordered[package]){
+        return;
+      }
+      mainPkg.push(package);
+      var pkg = indvlPkgs[package];
+      for(var i=0;i<pkg.length;i++){
+        sort(pkg[i], mainPkg);
+      }
+      ordered[package] = true;
+      output.push(package);
+    }
+
+    return output;
+  }
+
+  return {
+    dependencyCheck: function() {
+      var indvlPkgs = individualPackages();
+      return orderOfInstall(indvlPkgs).join(', ');
+    }
   };
+};
